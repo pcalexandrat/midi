04739b4cffb736aa66ff0057d80f226d4363d78d
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2023-10-18 23:28:25 -0300

    tecla

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ function tocaSom (idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorall('.tecla_pom').onclick = tocaSomPom;
-
+const listaDeTeclas = document.querySelectorall('.tecla');
+    
 let contador = 0
 
 while (contador < listaDeTeclas.length) {
-listaDeTeclas[contador].onclick = tocaSom;
+//template string
+    const tecla = listaDeTeclas[contador];  
+    const instrumento = tecla.classList[1];  
+    const idAudio = `#som_${instrumento}`;
+
+console.log (idAudio);
+
+
+tecla.onclick = function () {
+tocaSom ('idAudio')
+}
 contador = contador + 1;
 console.log (contador);
 }
