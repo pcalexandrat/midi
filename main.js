a33d695c1db2deaaaf4a3398f2a864578c250871
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2023-10-18 22:51:42 -0300

    teste parâmetros

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert('Ola mundo');
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorall('.tecla_pom').onclick = tocaSomPom;
+
+let contador = 0
+
+while (contador < listaDeTeclas.length) {
+listaDeTeclas[contador].onclick = tocaSom;
+contador = contador + 1;
+console.log (contador);
+}
