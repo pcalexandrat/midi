39d4742634ba5d0e68f5a6514bdf6999cf1614e4
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2023-11-15 22:02:01 -0300

    selectorAudio

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
     if (elemento && elemento.localName === 'audio') {
-        elemento.play(), alert('audio');
+        elemento.play(), alert('selectorAudio');
     }
     else {
         //alert('Elemento não encontrado'); 
