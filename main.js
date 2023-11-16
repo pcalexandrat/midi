9fd83b6de6f61ca6270def90ed79935309542c42
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2023-11-15 22:20:36 -0300

    button

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function tocaSom (seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
     if (elemento && elemento.localName === 'audio') {
-        elemento.play(), alert(GamepadButton);
+        elemento.play(), alert(button);
     }
     else {
         //alert('Elemento não encontrado'); 
