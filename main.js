3c41f0956b453c07af9d99c6b7f67f6a0742974f
Alexandra <127262337+pcalexandrat@users.noreply.github.com>
2023-11-16 14:13:04 -0300

    crase

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = '#som_${instrumento}'; //template string
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
